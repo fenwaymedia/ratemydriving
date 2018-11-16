0ef36c06d4fabee791a5a007e58d32d45d361e7b
Nick Drishinski <ndrishinski@yahoo.com>
2018-11-15 22:04:30 -0700

    adding rest api

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const product = require('./routes/product.routes'); // Imports routes for the products
+const app = express();
+app.use('/products', product);
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://someuser:1234asdf@ds037077.mlab.com:37077/nicksproductutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// var bodyParser = require('body-parser');
+//
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.use(bodyParser.json());
+// //
+// // router.get('/', function(req, res) {
+// //     res.json({ message: 'hooray! welcome to our api!' });
+// // });
+//
+// app.post('/', function(req, res) {
+//     res.send('this is a post request')
+//   });
+// app.get('/', (req, res) => res.send('Hi Nick, Welcome'))
+//
+// app.get('/nick', (req, res) => res.send('Hi Nick, Welcome'))
+//
+// app.get('/clark', (req, res) => res.send('Clarks what happens when I just save this'))
+//
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
